4411e33ebf453f03a49f7035df3339292adc0525
Anna Sholudko <anyasholudko19@gmail.com>
2018-12-27 12:25:50 +0200

    zalik

---
+++ zalik/Database.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data.Odbc;
+
+namespace zalik
+{
+    class Database
+    {
+        public string ConnectionString { get; set; }
+        public Database(string Con) { this.ConnectionString = Con; }
+        public SqlConnection cn;
+        public void Task1()
+        {
+            cn = new SqlConnection(ConnectionString);
+            cn.Open();
+            Console.WriteLine("1.");
+            string strSql = "select k.id,k.name,k.language,city.name from kind_of_inhabitant k inner join city on city.id_city=k.id_city where k.language='ukrainian' and city.name='Kyiv';";
+            SqlCommand com = new SqlCommand(strSql, cn);
+            SqlDataReader dr = com.ExecuteReader();
+            while (dr.Read())
+            {
+                Console.WriteLine(dr[0] + " " + dr[1] + " " + dr[2] + " " + dr[3]);
+            }
+            dr.Close();
+            cn.Close();
+        }
+        public void Task2()
+        {
+            cn = new SqlConnection(ConnectionString);
+            cn.Open();
+            Console.WriteLine("2.");
+            string strSql1 = "select city.id_city,city.name,city.square,city.population from kind_of_inhabitant k inner join city on city.id_city=k.id_city where k.name='children';";
+            SqlCommand com1 = new SqlCommand(strSql1, cn);
+            SqlDataReader dr1 = com1.ExecuteReader();
+            while (dr1.Read())
+            {
+                Console.WriteLine(dr1[0] + " " + dr1[1] + " " + dr1[2] + " " + dr1[3]);
+            }
+            dr1.Close();
+            cn.Close();
+        }
+        public void Task3()
+        {
+            cn = new SqlConnection(ConnectionString);
+            cn.Open();
+            Console.WriteLine("3");
+            string strSql2 = "select city.id_city,city.name,city.square,city.population, k.id,k.name from kind_of_inhabitant k inner join city on city.id_city=k.id_city where city.population=2000000;";
+            SqlCommand com2 = new SqlCommand(strSql2, cn);
+            SqlDataReader dr2 = com2.ExecuteReader();
+            while (dr2.Read())
+            {
+                Console.WriteLine(dr2[0] + " " + dr2[1] + " " + dr2[2] + " " + dr2[3] + " " + dr2[4] + " " + dr2[5]);
+            }
+            dr2.Close();
+            cn.Close();
+        }
+        public void Task4()
+        {
+            
+            cn = new SqlConnection(ConnectionString);
+            cn.Open();
+            Console.WriteLine("4");
+            string strSql3 = "select * from kind_of_inhabitant where name='pensioner';";
+            SqlCommand com3 = new SqlCommand(strSql3, cn);
+            SqlDataReader dr3 = com3.ExecuteReader();
+            while (dr3.Read())
+            {
+                Console.WriteLine(dr3[0] + " " + dr3[1] + " " + dr3[2] + " " + dr3[3]);
+            }
+            dr3.Close();
+            cn.Close();
+        }
+    }
+}
---
+++ zalik/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data.Odbc;
+
+namespace zalik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database db = new Database(@"Data Source=(local);Initial catalog=zalik;Integrated Security=True");
+            db.Task1();
+            db.Task2();
+            db.Task3();
+            db.Task4();
+            Console.ReadKey();
+        }
+    }
+}
